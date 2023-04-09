bfc530ad7036b27c6c0a39694d946dd99fd83a30
JJisak <76518907+JJisak@users.noreply.github.com>
2023-04-09 16:12:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -12,11 +14,29 @@ public class Main {
         FileWriter writer3 = new FileWriter("course3.csv");
 
         String line = reader.readLine();
+        List<String[]> allStudents = new ArrayList<>();
 
         while ((line = reader.readLine()) != null) {
             String[] parts = line.split(",");
+            allStudents.add(parts);
+        }
 
-            String courseName = parts[2].split(" ")[0];
+        Collections.sort(allStudents, new Comparator<String[]>() {
+            public int compare(String[] s1, String[] s2) {
+                int cmp = s1[2].compareTo(s2[2]);
+                if (cmp == 0) {
+                    return Integer.compare(Integer.parseInt(s2[3]), Integer.parseInt(s1[3]));
+                }
+                return cmp;
+            }
+        });
+
+        int course1Count = 0;
+        int course2Count = 0;
+        int course3Count = 0;
+
+        for (String[] student : allStudents) {
+            String courseName = student[2].split(" ")[0];
             int course;
 
             switch (courseName) {
@@ -30,61 +50,42 @@ public class Main {
                     course = 3;
                     break;
                 default:
-                    throw new IllegalArgumentException("Invalid course: " + parts[2]);
+                    throw new IllegalArgumentException("Invalid course: " + student[2]);
             }
 
-            String[][] students = new String[100][4];
-            int studentCount = 0;
-            students[studentCount++] = parts;
-
-            while ((line = reader.readLine()) != null && line.split(",")[2].startsWith(courseName)) {
-                students[studentCount++] = line.split(",");
+            switch (course) {
+                case 1:
+                    writeStudent(writer1, student);
+                    course1Count++;
+                    break;
+                case 2:
+                    writeStudent(writer2, student);
+                    course2Count++;
+                    break;
+                case 3:
+                    writeStudent(writer3, student);
+                    course3Count++;
+                    break;
             }
+        }
 
-            Arrays.sort(students, 0, studentCount, new Comparator<String[]>() {
-                public int compare(String[] s1, String[] s2) {
-                    return Integer.compare(Integer.parseInt(s2[3]), Integer.parseInt(s1[3]));
-                }
-            });
-
-            for (int i = 0; i < studentCount; i++) {
-                String[] student = students[i];
-
-                switch (course) {
-                    case 1:
-                        for (int j = 0; j < student.length; j++) {
-                            writer1.write(student[j]);
-                            if (j < student.length - 1) {
-                                writer1.write(",");
-                            }
-                        }
-                        writer1.write("\n");
-                        break;
-                    case 2:
-                        for (int j = 0; j < student.length; j++) {
-                            writer2.write(student[j]);
-                            if (j < student.length - 1) {
-                                writer2.write(",");
-                            }
-                        }
-                        writer2.write("\n");
-                        break;
-                    case 3:
-                        for (int j = 0; j < student.length; j++) {
-                            writer3.write(student[j]);
-                            if (j < student.length - 1) {
-                                writer3.write(",");
-                            }
-                        }
-                        writer3.write("\n");
-                        break;
-                }
-            }
+        if (course1Count + course2Count + course3Count != 100) {
+            throw new IllegalStateException("count doesn't match");
         }
 
         reader.close();
         writer1.close();
         writer2.close();
         writer3.close();
+    }
+
+    private static void writeStudent(FileWriter writer, String[] student) throws Exception {
+        for (int i = 0; i < student.length; i++) {
+            writer.write(student[i]);
+            if (i < student.length - 1) {
+                writer.write(",");
+            }
+        }
+        writer.write("\n");
     }
 }
